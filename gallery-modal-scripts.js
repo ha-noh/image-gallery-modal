4a371da8e7e5cf905648e388ca7bc2a177839603
Andrew <7147407+ha-noh@users.noreply.github.com>
2019-09-20 17:00:35 -0400

    initial working modal
    +gallery modal overlay opens on click
    +removed null pointer errors from js running before DOM was ready
    +added temporary content  (html, /images, /css) for testing

--- gallery-modal-scripts.js
+++ gallery-modal-scripts.js
@@ -1,1 +1,51 @@
-// also support arrow key navigation
+function run() {
+	//save the NodeList of all gallery images globally
+	const galleryImages = document.querySelectorAll('.gallery-modal-ready img');
+
+	//gallery is closed by default, and so no valid image index is available
+	let currentImageIndex = -1;
+
+	// event listener; also support arrow key navigation
+	// not sure if this works with <picture> elements
+	document.querySelector('.gallery-modal-ready').addEventListener('click', evt => openGalleryModal(evt));
+}
+
+// in case the document is already rendered
+if (document.readyState!='loading') run();
+// modern browsers
+else if (document.addEventListener) document.addEventListener('DOMContentLoaded', run);
+// IE <= 8
+else document.attachEvent('onreadystatechange', function(){
+    if (document.readyState=='complete') run();
+});
+
+function openGalleryModal(evt) {
+	if(!(evt.target.nodeName === 'IMG')) return;
+
+	document.querySelector('#gallery-modal-image').src = evt.target.src;
+	document.querySelector('.gallery-modal-container').style.display = 'block';
+}
+
+function closeGalleryModal() {
+
+}
+
+function nextGalleryImage() {
+
+}
+
+function loadGalleryImage() {
+
+}
+
+function prevGalleryImage() {
+
+}
+
+function updateGalleryIndex(index, totalCount) {
+
+}
+
+function updateModalTabindex() {
+
+}
