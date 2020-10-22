3b9d31bcc6e3f067a57fedf2d7c71be6d39369ee
Andrew <7147407+ha-noh@users.noreply.github.com>
2020-10-21 23:45:59 -0400

    BEM naming convention
    + The codebase has been refactored to follow the BEM naming convention; this should reduce the chance of conflict with other sources and hopefully make it more readable for humans.
    
    There is NO CHANGE to how the module is called on the user end

--- gallery-modal-scripts.js
+++ gallery-modal-scripts.js
@@ -2,8 +2,9 @@
  * (1) Find a more efficient solution for determining the gallery index number (faster than linear runtime)
  *
  * HOW TO USE: 
- * Include this file at the bottom of your webpage, so it runs only after the DOM has loaded 
- * Ex: <script src="yourfolder/gallery-modal-scripts.js"></script>
+ * (1) Add the "gallery-modal-ready" HTML class to the HTML element that contains the images you wish to affect.
+ * (2) Include this file as a script element at the bottom of your webpage (so it only runs after the DOM has loaded), i.e.
+ * <script src="yourfolder/gallery-modal-scripts.js"></script>
  */
 const imageGalleryModal = (function() {
 	//gallery is closed by default, therefore the index is set to a negative value
@@ -24,8 +25,8 @@ const imageGalleryModal = (function() {
 		if(e.target.nodeName !== 'IMG') return;
 		
 		loadGalleryImage(e);
-		document.querySelector('.gallery-modal-container').classList.toggle('modal-display');
-		document.querySelector('.gallery-modal-container').focus();
+		document.querySelector('.gallery-modal__container').classList.toggle('gallery-modal_display');
+		document.querySelector('.gallery-modal__container').focus();
 		toggleGalleryImageFx();
 	};
 
@@ -53,7 +54,7 @@ const imageGalleryModal = (function() {
 
 	//opens an image based on the click event target
 	const loadGalleryImage = function(e) {
-		document.querySelector('.gallery-modal-image').src = e.target.src;
+		document.querySelector('.gallery-modal__image').src = e.target.src;
 
 		//iterate through images to find the current image's index (its position in order)
 		for(let x = 0; x < galleryImages.length; x++) {
@@ -67,7 +68,7 @@ const imageGalleryModal = (function() {
 	//opens an image based on its index in the gallery image list
 	const openGalleryImage = function() {
 		let newImgSrc = galleryImages.item(currentImageIndex).src;
-		document.querySelector('.gallery-modal-image').src = newImgSrc;
+		document.querySelector('.gallery-modal__image').src = newImgSrc;
 		
 		imageIndexDidUpdate();
 		showFeaturesIfHidden();
@@ -98,27 +99,27 @@ const imageGalleryModal = (function() {
 	//------Helper Functions------
 
 	function toggleGalleryImageFx() {
-		document.querySelector('.gallery-modal-image').classList.toggle('image-fx');
+		document.querySelector('.gallery-modal__image').classList.toggle('gallery-modal_image-fx');
 	}
 
 	function toggleModalDisplay() {
-		document.querySelector('.gallery-modal-container').classList.toggle('modal-display');
+		document.querySelector('.gallery-modal__container').classList.toggle('gallery-modal_display');
 	}
 
 	//hides or shows all of the gallery modal buttons and the modal index
 	function toggleModalFeatures() {
-		let buttons = document.querySelectorAll('.gallery-modal-button');
+		let buttons = document.querySelectorAll('.gallery-modal__button');
 		for(const button of buttons) {
-			button.classList.toggle('modal-features-hide');
+			button.classList.toggle('gallery-modal_hide-features');
 		}
-		document.querySelector('.gallery-modal-index').classList.toggle('modal-features-hide');
+		document.querySelector('.gallery-modal__index').classList.toggle('gallery-modal_hide-features');
 	}
 
 	function showFeaturesIfHidden() {
 		//no need to check every feature individually since they're always toggled together
-		let feature = document.querySelector('.gallery-modal-index');
+		let feature = document.querySelector('.gallery-modal__index');
 		//show features if they're currently hidden
-		if(feature.classList.contains('modal-features-hide')) {
+		if(feature.classList.contains('gallery-modal_hide-features')) {
 			toggleModalFeatures();
 			return true;
 		}
@@ -146,26 +147,26 @@ const imageGalleryModal = (function() {
 	function imageIndexDidUpdate() {
 		const imageIndexString = `${currentImageIndex + 1} / ${galleryImages.length}`;
 		//console.log(imageIndexString);
-		document.querySelector('.gallery-modal-index').innerText = imageIndexString;
+		document.querySelector('.gallery-modal__index').innerText = imageIndexString;
 	};
 
 
 	//------ Initialize listeners on buttons and gallery ------
 
 	//modal buttons
-	document.querySelector('.modal-close-button').addEventListener('click', closeGalleryModal);
-	document.querySelector('.modal-close-button').addEventListener('focus', showFeaturesIfHidden);
+	document.querySelector('.gallery-modal__button_close').addEventListener('click', closeGalleryModal);
+	document.querySelector('.gallery-modal__button_close').addEventListener('focus', showFeaturesIfHidden);
 
-	document.querySelector('.modal-prev-button').addEventListener('click', prevGalleryImage);
-	document.querySelector('.modal-prev-button').addEventListener('focus', showFeaturesIfHidden);
+	document.querySelector('.gallery-modal__button_prev').addEventListener('click', prevGalleryImage);
+	document.querySelector('.gallery-modal__button_prev').addEventListener('focus', showFeaturesIfHidden);
 
-	document.querySelector('.modal-next-button').addEventListener('click', nextGalleryImage);
-	document.querySelector('.modal-next-button').addEventListener('focus', showFeaturesIfHidden);
+	document.querySelector('.gallery-modal__button_next').addEventListener('click', nextGalleryImage);
+	document.querySelector('.gallery-modal__button_next').addEventListener('focus', showFeaturesIfHidden);
 
-	document.querySelector('.gallery-modal-image').addEventListener('click', toggleModalFeatures);
-	document.querySelector('.gallery-modal-image').addEventListener('mouseout', showFeaturesIfHidden);
+	document.querySelector('.gallery-modal__image').addEventListener('click', toggleModalFeatures);
+	document.querySelector('.gallery-modal__image').addEventListener('mouseout', showFeaturesIfHidden);
 	
-	document.querySelector('.invisible-button').addEventListener('click', toggleModalFeatures);
+	document.querySelector('.gallery-modal__button_invisible').addEventListener('click', toggleModalFeatures);
 
 	//listen for clicks on gallery elements
 	readyGalleryListener();
